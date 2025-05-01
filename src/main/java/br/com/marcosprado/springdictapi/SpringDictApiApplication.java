6a276019de3345ae1e8feef062602e0e1409f2cb
Marcos Prado <marcospradofelix@gmail.com>
2025-05-01 12:26:55 -0300

    Initial commit

---
+++ src/main/java/br/com/marcosprado/springdictapi/SpringDictApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcosprado.springdictapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDictApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDictApiApplication.class, args);
+    }
+
+}
